177326b6c9082949f7f3657ef571e59344cc75dc
Foxy224 <ppaaa4@gmail.com>
2023-05-19 19:09:33 +0300

    123

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+
+import java.sql.*;
+
+public class Util  {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Zmalqp1092";
+
+    Connection connection;
+
+    public Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
 }
+
